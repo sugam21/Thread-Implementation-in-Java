7cd6a91bb985e3d59c2cdcf95002f14dfdd1439a
Sugam Sharma <87814957+sugam21@users.noreply.github.com>
2023-04-29 22:33:36 +0530

    second commit

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -12,10 +12,12 @@ class Printer extends Thread{
         {
             try {
                 Thread.sleep(500);
+
             } catch (InterruptedException e) {
                 e.getStackTrace();
             }
             System.out.print(ch);
+
         }
     }
 }
@@ -23,17 +25,17 @@ public class ThreadDemo {
     public static void main(String[] args) {
         // Creating instance of Printer class.--------------------------------->
         Printer printer = new Printer();
-        printer.setDaemon(true); // This will make the thread demoen which will exit on exitting the main method.
+        printer.setName("Thread1"); 
         printer.setMessage("Hello, how are you ?"); // Calling the setMessage function of the Printer class.
         System.out.println(printer.getName());
         printer.start(); // Thread will start
 
+
         // Creating another instance of the Printer class.---------------------------->
         Printer anotherPrinter = new Printer();
-        anotherPrinter.setDaemon(true);
+        anotherPrinter.setName("Thread2");
         anotherPrinter.setMessage("I am fine");
         anotherPrinter.start();
-        System.out.println(anotherPrinter.getName());
         System.out.println("End of Main"); // This is printed first because after this main method dies and non demon thread continuous to work even after the main method dies.
 
     }
